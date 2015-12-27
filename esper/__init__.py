fd8e27936a3bccc9844e368adb3df9b82f37391e
Benjamin <benmoran56@gmail.com>
2015-12-27 22:47:27 +0900

    Minor fix in __init__.

--- esper/__init__.py
+++ esper/__init__.py
@@ -1,9 +1,9 @@
 from .world import World
 from .templates import Processor
 
-__version__ = "0.3"
+__version__ = "0.4"
 __author__ = None
 __license__ = "MIT"
 __copyright__ = "Benjamin Moran"
 
-__all__ = ["World", "Processor", "Component"]
+__all__ = ["World", "Processor"]
