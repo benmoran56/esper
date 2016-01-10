836f9c228a673625c9a5c9eaf11230da1f60b680
Christopher Arndt <chris@chrisarndt.de>
2016-01-10 10:13:55 +0100

    Add setuptools support and distribution meta data

--- esper/__init__.py
+++ esper/__init__.py
@@ -1,9 +1,8 @@
 from .world import CachedWorld, World
 from .templates import Processor
 
-__version__ = "0.9.2"
-__author__ = "Benjamin Moran"
-__license__ = "MIT"
-__copyright__ = "Benjamin Moran"
+from .meta import (author as __author__, version as __version__,
+                   license as __license__)
+__copyright__ = __author__
 
 __all__ = ("CachedWorld", "Processor", "World")
