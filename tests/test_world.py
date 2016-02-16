a369340653e4a7b574136bf3263ad8fd17717b43
Benjamin Moran <benmoran56@gmail.com>
2016-02-16 17:03:18 +0900

    Most methods are now covered by unit tests.

--- tests/test_world.py
+++ tests/test_world.py
@@ -24,14 +24,36 @@ def test_world_instantiation(world):
     assert type(world._processors) == list
 
 
-def test_entity_creation(world):
+def test_create_entity(world):
     entity1 = world.create_entity()
     entity2 = world.create_entity()
     assert type(entity1) and type(entity2) == int
     assert entity1 < entity2
 
 
-def test_single_component_query(populated_world):
+def test_delete_entity(world):
+    entity1 = world.create_entity()
+    world.add_component(entity1, ComponentC())
+    entity2 = world.create_entity()
+    world.add_component(entity2, ComponentD())
+    entity3 = world.create_entity()
+    world.add_component(entity3, ComponentE())
+
+    assert entity3 == 3
+    world.delete_entity(3)
+    # TODO: handle case where entity has never been assigned components
+    with pytest.raises(KeyError):
+        world.delete_entity(999)
+
+
+def test_component_for_entity(world):
+    entity = world.create_entity()
+    world.add_component(entity, ComponentC())
+    assert isinstance(world.component_for_entity(entity, ComponentC), ComponentC)
+    assert not world.component_for_entity(entity, ComponentD)
+
+
+def test_get_component(populated_world):
     assert isinstance(populated_world.get_component(ComponentA), types.GeneratorType)
 
     for ent, comp in populated_world.get_component(ComponentA):
@@ -39,7 +61,7 @@ def test_single_component_query(populated_world):
         assert type(comp) == ComponentA
 
 
-def test_dual_component_query(populated_world):
+def test_get_two_components(populated_world):
     assert isinstance(populated_world.get_components(ComponentD, ComponentE),
                       types.GeneratorType)
 
@@ -54,7 +76,7 @@ def test_dual_component_query(populated_world):
         assert type(e) == ComponentE
 
 
-def test_triple_component_query(populated_world):
+def test_get_three_components(populated_world):
     assert isinstance(populated_world.get_components(ComponentC, ComponentD, ComponentE),
                       types.GeneratorType)
 
@@ -70,10 +92,39 @@ def test_triple_component_query(populated_world):
         assert type(e) == ComponentE
 
 
+def test_clear_database(populated_world):
+    populated_world.clear_database()
+    assert len(populated_world._entities) == 0
+    assert len(populated_world._components) == 0
+    assert len(populated_world._processors) == 0
+    assert populated_world._next_entity_id == 0
+
+
+def test_add_processor(populated_world):
+    assert len(populated_world._processors) == 0
+    correct_processor_a = CorrectProcessorA()
+    assert isinstance(correct_processor_a, esper.Processor)
+    populated_world.add_processor(correct_processor_a)
+    assert len(populated_world._processors) == 1
+    assert isinstance(populated_world._processors[0], esper.Processor)
+
+
+def test_remove_processor(populated_world):
+    assert len(populated_world._processors) == 0
+    correct_processor_a = CorrectProcessorA()
+    populated_world.add_processor(correct_processor_a)
+    assert len(populated_world._processors) == 1
+    populated_world.remove_processor(CorrectProcessorB)
+    assert len(populated_world._processors) == 1
+    populated_world.remove_processor(CorrectProcessorA)
+    assert len(populated_world._processors) == 0
+
+
+
+
 ##################################################
 #   Some helper functions and Component templates:
 ##################################################
-
 def create_entities(world, number):
     for _ in range(number // 2):
         enemy_type_a = world.create_entity()
@@ -118,3 +169,25 @@ class ComponentE:
     def __init__(self):
         self.items = {"itema": None, "itemb": 1000}
         self.points = [a + 2 for a in list(range(44))]
+
+
+class CorrectProcessorA(esper.Processor):
+    def __init__(self):
+        super().__init__()
+
+    def process(self):
+        pass
+
+class CorrectProcessorB(esper.Processor):
+    def __init__(self):
+        super().__init__()
+
+    def process(self):
+        pass
+
+class IncorrectProcessor:
+    def __init__(self):
+        super().__init__()
+
+    def process(self):
+        pass
