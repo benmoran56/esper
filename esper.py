250db5205de078f381d80f1277c3f8a2b7f78b8d
Anas Elmi <elmi.anas@gmail.com>
2018-06-06 15:56:18 -0700

    added clarifying comments and duplicated entiy deletion

--- esper.py
+++ esper.py
@@ -111,7 +111,9 @@ class World:
         :param entity: The Entity ID you wish to delete.
         :param immediate: If True, delete the Entity immediately.
         """
+
         if immediate:
+            # If you modify this, make sure you reflect changes in _clear_dead_entities
             for component_type in self._entities[entity]:
                 self._components[component_type].discard(entity)
 
@@ -248,10 +250,18 @@ class World:
                 raise StopIteration
 
     def _clear_dead_entities(self):
-        if self._dead_entities:
-            for entity in self._dead_entities:
-                self.delete_entity(entity, immediate=True)
-            self._dead_entities.clear()
+        # Should mirror delete_entity
+        for entity in self._dead_entities:
+
+            for component_type in self._entities[entity]:
+                self._components[component_type].discard(entity)
+
+                if not self._components[component_type]:
+                    del self._components[component_type]
+
+            del self._entities[entity]
+
+        self._dead_entities.clear()
 
     def _process(self, *args):
         for processor in self._processors:
